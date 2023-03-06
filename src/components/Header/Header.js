7fd66278aac8315d866fcdd9c96e986d20127de5
shaafiiull <thisisshaafii@gmail.com>
2023-03-07 00:54:38 +0600

    header file and tailwind installation done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-3xl font-bold underline">
+        My Basic Standard Function Calculator
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
